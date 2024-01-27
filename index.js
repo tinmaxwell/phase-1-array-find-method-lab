0bf41d7ec7a0daaa8a25d24151c6dd9f77def027
tinmaxwell <tinmaxwell@gmail.com>
2024-01-27 12:18:12 -0600

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(array) {
+    const win = array.find(obj => obj.result === "W");
+    if (win) {
+      return win.year;
+    }
+    return undefined;
+  }
